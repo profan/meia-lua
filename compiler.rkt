b65bbc1d3e38881e82994cf76dea05622245f757
Robin Hübner <profan@prfn.se>
2016-10-23 14:15:52 +0200

    compiler: testing defining passes, learning nanopass

--- compiler.rkt
+++ compiler.rkt
@@ -75,8 +75,50 @@
               [(string-prefix? str "\"") (token 'STR (string-trim str "\""))]
               [else (token 'VAR str)])]))])))
 
+(define (variable? x)
+  (symbol? x))
+
+(define (constant? x)
+  (or (number? x)
+      (string? x)
+      (char? x)))
+
+(define (operator? x)
+  (hash-has-key? operators x))
+
+(define (keyword? x)
+  (hash-has-key? keywords x))
+
+(define-language L0
+  (terminals
+   (constant (c))
+   (variable (x))
+   (operator (o)))
+  (Expr (e body)
+        c ;; constant
+        x ;; variable
+        (op o e0 e1))
+  (Stmt (s body)
+        (assign x e)))
+
+(define-language L1
+  (extends L0)
+  (Expr (e body)
+        (+
+         (op-assign o x e))))
+
+(define-pass lower-operations : L1 (ir) -> L0 ()
+  (definitions)
+  (Expr : Expr (ir) -> Stmt ()
+        [(op-assign ,o ,x ,[e]) `(assign x (op o x e))])
+  (Expr ir))
+
+(language->s-expression L0)
+(language->s-expression L1)
+
 (tokenize (open-input-string "for value, other_value in pairs({12, 24}) do 25 = 32 end"))
 (tokenize (open-input-string "function test() return \"Hello, World!\" end"))
 (tokenize (open-input-string "function test() local some_stuff = {1, 2, 3, 4}; return #some_stuff end"))
 (tokenize (open-input-string "function hello_world() return false end"))
 (tokenize (open-input-string "function hello_world() return not -25 end"))
+(tokenize (open-input-string "local thing = 25 + 35"))
